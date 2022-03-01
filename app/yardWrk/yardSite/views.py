27cfe230ec91203e654903090cf2f12e11a4a9bf
David Storey <david.storey@aggiemail.usu.edu>
2022-02-28 23:28:48 -0700

    There exists a customer page

--- app/yardWrk/yardSite/views.py
+++ app/yardWrk/yardSite/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from matplotlib.style import available
+from .models import *
 
 # Create your views here.
+def CustomerDashboard(request):
+    pending_jobs = Job.objects.filter(available=True).filter(completed=False)
+    progressing_jobs = Job.objects.filter(available=False).filter(completed=False)
+    completed_jobs = Job.objects.filter(completed=True)
+    return HttpResponse("Welcome to the Customer Dashboard!")
