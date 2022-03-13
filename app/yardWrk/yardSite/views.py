fe62805075be3f01c7770d765152ad2599f4028d
David Storey <david.storey@aggiemail.usu.edu>
2022-03-13 14:08:10 -0600

    I'm able to display simple data on Customer dash

--- app/yardWrk/yardSite/views.py
+++ app/yardWrk/yardSite/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import *
 
-# Create your views here.
+# Responsible for creating the view a user sees when navigating to their customer page.
 def CustomerDashboard(request):
     pending_jobs = Job.objects.filter(available=True).filter(completed=False)
     progressing_jobs = Job.objects.filter(available=False).filter(completed=False)
     completed_jobs = Job.objects.filter(completed=True)
-    return HttpResponse("Welcome to the Customer Dashboard!")
+
+    context = {
+        'pending_jobs': pending_jobs,
+        'progresesing_jobs': progressing_jobs,
+        'completed_jobs': completed_jobs,
+    }
+
+    return render(request, 'yardSite/customerDashboard.html', context)
