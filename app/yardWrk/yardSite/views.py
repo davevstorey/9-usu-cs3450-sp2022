aca790e5c8d2bea99e2496b5f965da102720bdd6
David Storey <david.storey@aggiemail.usu.edu>
2022-03-21 21:44:27 -0600

    Jobs should be filterd by logged in user

--- app/yardWrk/yardSite/views.py
+++ app/yardWrk/yardSite/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .models import *
 from .forms import JobPostForm
 
 # Responsible for creating the view a user sees when navigating to their customer page.
+
+@login_required(login_url='/accounts/login')
 def CustomerDashboard(request):
-    pending_jobs = Job.objects.filter(available=True).filter(completed=False)
-    progressing_jobs = Job.objects.filter(available=False).filter(completed=False)
-    completed_jobs = Job.objects.filter(completed=True)
+    # Grab the customer that is tied to the logged in user
+    currentUser = Customer.objects.filter(user=request.user)[0]
+    
+    pending_jobs = Job.objects.filter(available=True).filter(completed=False).filter(customer=currentUser)
+    progressing_jobs = Job.objects.filter(available=False).filter(completed=False).filter(customer=currentUser)
+    completed_jobs = Job.objects.filter(completed=True).filter(customer=currentUser)
 
     context = {
         'pending_jobs': pending_jobs,
