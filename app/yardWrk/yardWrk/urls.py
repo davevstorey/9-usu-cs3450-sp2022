cb2369f30d90e1a16adf431758ae8bd65000d9a8
jakepper <epperson.jacob@gmail.com>
2022-03-27 18:51:10 -0600

    created owner app; Added home page template

--- app/yardWrk/yardWrk/urls.py
+++ app/yardWrk/yardWrk/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('customer/', include('yardSite.urls')),
+    path('owner/', include('owner.urls')),
 ]
